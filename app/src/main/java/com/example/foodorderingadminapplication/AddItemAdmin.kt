c1ce009f33ef1881b4fb6709c27afc6189ac87df
bachduong42 <bachduongvan0402@gmail.com>
2024-05-20 08:36:51 +0700

    add function Menu management

--- app/src/main/java/com/example/foodorderingadminapplication/AddItemAdmin.kt
+++ app/src/main/java/com/example/foodorderingadminapplication/AddItemAdmin.kt
@@ -1,28 +1,105 @@
 package com.example.foodorderingadminapplication
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.foodorderingadminapplication.databinding.ActivityAddItemAdminBinding
+import com.example.foodorderingadminapplication.model.AllMenu
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class AddItemAdmin : AppCompatActivity() {
+
+    //food items detail
+    private lateinit var foodName: String
+    private lateinit var foodPrice: String
+    private lateinit var foodDescription: String
+    private var foodImage: Uri? = null
+    private lateinit var foodIngredient: String
+    //firebase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+
+
     private val binding:ActivityAddItemAdminBinding by lazy {
         ActivityAddItemAdminBinding.inflate(layoutInflater);
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root);
-        binding.selectedImage.setOnClickListener {
-            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly));
+        setContentView(binding.root)
+        //initialize FireBase
+        auth = FirebaseAuth.getInstance()
+        //initialize FireBase database instance
+        database = FirebaseDatabase.getInstance()
+
+        binding.btnAddItem.setOnClickListener {
+            //get data from Filed
+            foodName = binding.edtNameItem.text.toString().trim()
+            foodPrice = binding.edtPriceItem.text.toString().trim()
+            foodDescription = binding.etDescription.text.toString().trim()
+            foodIngredient = binding.etIngredients.text.toString().trim()
+
+            if (foodDescription.isBlank()||foodIngredient.isBlank()||foodName.isBlank()||foodPrice.isBlank()){
+                Toast.makeText(this,"Fill all details!", Toast.LENGTH_LONG).show()
+            } else{
+                uploadData()
+                Toast.makeText(this,"Item add successfully!", Toast.LENGTH_LONG).show()
+            }
         }
+        binding.btnSelectImage.setOnClickListener {
+            pickImage.launch("image/*")
+        }
+
+
         binding.btnBack.setOnClickListener{
             finish();
         }
     }
-    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
+
+    private fun uploadData() {
+        // get a reference to the "menu" node in the database
+        val menuRef = database.getReference("menu")
+        // generate a unique key for the new menu item
+        val newItemKey = menuRef.push().key
+        if(foodImage !=null){
+            val storageRef = FirebaseStorage.getInstance().reference
+            val imageRef = storageRef.child("menu_images/${newItemKey}.ipg")
+            val uploadTask = imageRef.putFile(foodImage!!)
+
+            uploadTask.addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener {
+                        downloadUri ->
+                    // create new item
+                    val newItem = AllMenu(
+                        foodName,foodPrice,foodDescription,downloadUri.toString(),foodIngredient
+                    )
+                    newItemKey?.let{
+                            key ->
+                        menuRef.child(key).setValue(newItem).addOnSuccessListener {
+                            Toast.makeText(this,"Data upload successfully!", Toast.LENGTH_LONG).show()
+                        }
+                            .addOnFailureListener {
+                                Toast.makeText(this,"Data upload Failed!", Toast.LENGTH_LONG).show()
+                            }
+                    }
+                }
+                    .addOnFailureListener {
+                        Toast.makeText(this,"Image upload Failed!", Toast.LENGTH_LONG).show()
+                    }
+            }
+        } else{
+            Toast.makeText(this,"Please select image", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
         if(uri!=null){
             binding.selectedImage.setImageURI(uri);
+            foodImage = uri
         }
 
     }
--- app/src/main/java/com/example/foodorderingadminapplication/AllItemActivity.kt
+++ app/src/main/java/com/example/foodorderingadminapplication/AllItemActivity.kt
@@ -2,31 +2,72 @@ package com.example.foodorderingadminapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.foodorderingadminapplication.adapter.AddItemAdapter
+import com.example.foodorderingadminapplication.adapter.MenuItemAdapter
 import com.example.foodorderingadminapplication.databinding.ActivityAllItemBinding
+import com.example.foodorderingadminapplication.model.AllMenu
+import com.example.foodorderingadminapplication.model.UserModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AllItemActivity : AppCompatActivity() {
+
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private var menuItems: ArrayList<AllMenu> = ArrayList()
+    private lateinit var userModel: UserModel
+
     private val binding: ActivityAllItemBinding by lazy{
         ActivityAllItemBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val menuFoodName = listOf("Burger", "Pizza", "Sandwich","Burger", "Pizza", "Sandwich")
-        val menuFoodRestaurant = listOf("Con Toc", "Mina Restaurant", "Hiign Min","Valeing", "Kim Quy Restaurant", "BBQ")
-        val menuFoodPrice = listOf("$ 30", "$ 50", "$ 60","$ 70", "$ 30", "$ 80")
-        val menuFoodImage = listOf(
-            R.drawable.pizza,R.drawable.pizza,R.drawable.pizza,
-            R.drawable.pizza,R.drawable.pizza,R.drawable.pizza
-        )
-        val adapter = AddItemAdapter(ArrayList(menuFoodName), ArrayList(menuFoodRestaurant),
-            ArrayList(menuFoodPrice),ArrayList(menuFoodImage))
-        binding.rvItems.layoutManager= LinearLayoutManager(this)
-        binding.rvItems.adapter= adapter
+
+        var user = intent.getParcelableExtra<UserModel>("user")
+        userModel = UserModel(user?.email, user?.name,user?.nameOfRestaurant, user?.password)
+
+        databaseReference = FirebaseDatabase.getInstance().reference
+        retrivieveMenuItem()
 
         binding.btnBack.setOnClickListener{
             finish();
         }
+    }
+
+    private fun retrivieveMenuItem() {
+        database = FirebaseDatabase.getInstance()
+        val foodRef = database.reference.child("menu")
+        //fetch data from database
+        foodRef.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear existing data
+                menuItems.clear()
+                //loop for through each food item
+                for (foodSnapshot in snapshot.children){
+                    val menuItem = foodSnapshot.getValue(AllMenu::class.java)
+                    menuItem?.let {
+                        menuItems.add(it)
+                        setAdapter()
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("databaseError","Error: ${error.message}")
+            }
+        })
+    }
+
+    private fun setAdapter() {
+
+        val adapter = MenuItemAdapter(menuItems,this@AllItemActivity,databaseReference, userModel.nameOfRestaurant!!)
+
+        binding.rvItems.layoutManager= LinearLayoutManager(this)
+        binding.rvItems.adapter= adapter
     }
 }
