1be7011433126c4ac2247a506dda174ad9c90ede
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2024-06-07 09:01:23 +0700

    update func onCreate

--- app/src/main/java/com/example/foodorderingadminapplication/SignUpAdminActivity.kt
+++ app/src/main/java/com/example/foodorderingadminapplication/SignUpAdminActivity.kt
@@ -2,11 +2,44 @@ package com.midterm.foododeringapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.foodorderingadminapplication.R
+import com.example.foodorderingadminapplication.databinding.ActivitySignUpAdminBinding
 
 class SignUpAdminActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var userName: String
+    private lateinit var nameOfRestaurant: String
+    private lateinit var database: DatabaseReference
+
+    private val binding: ActivitySignUpAdminBinding by lazy{
+        ActivitySignUpAdminBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_admin)
+        setContentView(binding.root)
+        //initialize Firebase Auth
+        auth = Firebase.auth
+        //initialize Firebase database
+        database = Firebase.database.reference
+
+        binding.btnSignupAdmin.setOnClickListener {
+            email = binding.etEmail.text.toString().trim()
+            password = binding.etPassword.text.toString().trim()
+            userName = binding.etNameOwner.text.toString().trim()
+            nameOfRestaurant = binding.etNameRestaurant.text.toString().trim()
+
+            if (userName.isBlank() || email.isBlank() || password.isBlank() || nameOfRestaurant.isBlank()){
+                Toast.makeText(this, "Please fill all details", Toast.LENGTH_LONG).show()
+            } else{
+                createAccount(email, password)
+            }
+
+        }
+
     }
+
 }
