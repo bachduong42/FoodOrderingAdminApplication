adbb52c95ad50a60a0ce4c28d2b17bc76eeb00e3
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2024-06-07 09:03:32 +0700

    create Account

--- app/src/main/java/com/example/foodorderingadminapplication/SignUpAdminActivity.kt
+++ app/src/main/java/com/example/foodorderingadminapplication/SignUpAdminActivity.kt
@@ -1,8 +1,11 @@
 package com.midterm.foododeringapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.example.foodorderingadminapplication.LoginAdminActivity
 import com.example.foodorderingadminapplication.R
 import com.example.foodorderingadminapplication.databinding.ActivitySignUpAdminBinding
 
@@ -40,6 +43,21 @@ class SignUpAdminActivity : AppCompatActivity() {
 
         }
 
+    }
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(this, "Account created successfully", Toast.LENGTH_LONG).show()
+                saveUserData()
+                val intent = Intent(this, LoginAdminActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else{
+                Toast.makeText(this, "Account creation failed", Toast.LENGTH_LONG).show()
+                Log.d("Account","createAccount: Failure", it.exception)
+            }
+        }
     }
 
 }
