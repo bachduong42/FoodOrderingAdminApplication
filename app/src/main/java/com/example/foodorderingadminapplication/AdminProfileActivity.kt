2feb4953cda8a2f11204af60bfe444e5293a3b5e
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2024-06-07 08:28:31 +0700

    edit func onCreate AdminProfile

--- app/src/main/java/com/example/foodorderingadminapplication/AdminProfileActivity.kt
+++ app/src/main/java/com/example/foodorderingadminapplication/AdminProfileActivity.kt
@@ -8,12 +8,28 @@ class AdminProfileActivity : AppCompatActivity() {
     private val binding: ActivityAdminProfileBinding by lazy{
         ActivityAdminProfileBinding.inflate(layoutInflater)
     }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var adminReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        val adminId = auth.currentUser?.uid?:""
+        adminReference = database.reference.child("user").child(adminId)
+
+        binding.btnSaveInfor.setOnClickListener {
+            updateAdminData()
+        }
+
         binding.btnBack.setOnClickListener{
             finish();
         }
+        retrieveUserData()
     }
+
+
 }
